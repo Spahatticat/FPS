e80ae0e9e3774ddc6771abd16400e17fe8decaf1
Robby <spahatticat246@gmail.com>
2026-03-21 10:22:19 -0400

    dfgsdfgsf

---
+++ CMakeFiles/fps.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fps.dir/src/Engine/Camera.cpp.obj"
+  "CMakeFiles/fps.dir/src/Engine/Camera.cpp.obj.d"
+  "CMakeFiles/fps.dir/src/Engine/Entity.cpp.obj"
+  "CMakeFiles/fps.dir/src/Engine/Entity.cpp.obj.d"
+  "CMakeFiles/fps.dir/src/Engine/Shader.cpp.obj"
+  "CMakeFiles/fps.dir/src/Engine/Shader.cpp.obj.d"
+  "CMakeFiles/fps.dir/src/glad.c.obj"
+  "CMakeFiles/fps.dir/src/glad.c.obj.d"
+  "CMakeFiles/fps.dir/src/main.cpp.obj"
+  "CMakeFiles/fps.dir/src/main.cpp.obj.d"
+  "bin/fps.exe"
+  "bin/fps.exe.manifest"
+  "bin/fps.pdb"
+  "libfps.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/fps.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
